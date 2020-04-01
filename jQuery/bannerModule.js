b9de1805b3a73240a3483adab15738b382b8fba7
張煒杰 <jasonwchang@liontravel.com>
2020-04-01 17:02:51 +0800

    init

---
+++ jQuery/bannerModule.js
@@ -0,0 +1,13 @@
+(function($){
+    'use strict';
+    var ModuleName = 'banner';
+
+    var Module = function Module(element, options) {
+        this.ele = element;
+        this.$ele = $(element);
+        this.option = options;
+    };
+})(jQuery)
+
+
+// 一個js檔案引jQ
